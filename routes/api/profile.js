b79caf966cabcb27004b909778f8f7ca6db1294e
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-01 23:18:34 +0200

    Commit: Added the functionality needed to create and update profiles!]

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,4 +28,51 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+// @route  POST api/profile
+// @desc   Create or edit user profile
+// @access Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Get fields
+  const profileFields = {};
+  profileFields.social = {};
+  profileFields.user = req.user.id;
+  if (req.body.handle) profileFields.handle = req.body.handle;
+  if (req.body.company) profileFields.company = req.body.company;
+  if (req.body.location) profileFields.location = req.body.location;
+  if (req.body.bio) profileFields.bio = req.body.bio;
+  if (req.body.status) profileFields.status = req.body.status;
+  if (req.body.githubUsername) profileFields.githubUsername = req.body.githubUsername;
+  if (req.body.handle) profileFields.handle = req.body.handle;
+  if (typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',');
+  };
+  if (req.body.youtube) profileFields.youtube = req.body.social.youtube;
+  if (req.body.linkedin) profileFields.linkedin = req.body.social.linkedin;
+  if (req.body.twitter) profileFields.twitter = req.body.social.twitter;
+  if (req.body.facebook) profileFields.facebook = req.body.social.facebook;
+  if (req.body.instagram) profileFields.instagram = req.body.social.instagram;
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+          .then(profile => res.json(profile));
+      } else {
+        // Create
+        // Check if the handle exists
+        Profile.findOne({ handle: profileFields.handle })
+          .then(profile => {
+            if (profile) {
+              errors.handle = 'That handle already exists.';
+              res.status(400).json(errors);
+            }
+            // Save Profile
+            new Profile(profileFields)
+              .save()
+              .then(profile => res.json(profile));
+          });
+      }
+    });
+});
+
 module.exports = router;
