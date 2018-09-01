86307ff1b8083f2ac47813e1553fd11e04c0e76e
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-01 20:25:13 +0200

    Initial commit - Express Server w/ route files.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://database:database0@ds141872.mlab.com:41872/coursedatabase'
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+// Database
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('Database Connected.'))
+  .catch(err => console.log(err));
+
+// Router
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+// Server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
