3c1774564b1f0a8d207cec2deff5a8533ef1f295
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-06 22:11:32 +0200

    Commit: Added the ability to remove profiles and profile details!

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -179,4 +179,39 @@ router.post('/education', passport.authenticate('jwt', { session: false}), (req,
     });
 });
 
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false}), (req, res) => {
+Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    profile.save().then(profile => res.json(profile));
+  });
+});
+
+// @route  DELETE api/profile/education/:edu_id
+// @desc   Delete education from profile
+// @access Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false}), (req, res) => {
+Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    profile.save().then(profile => res.json(profile));
+  });
+});
+
+// @route  DELETE api/profile
+// @desc   Delete user and profile
+// @access Private
+router.delete('/', passport.authenticate('jwt', { session: false}), (req, res) => {
+Profile.findOneAndRemove({ user: req.user.id })
+  .then(() => {
+    User.findOneAndRemove({ _id: req.user.id })
+      .then(() => res.json({ success: true }))
+  })
+});
+
 module.exports = router;
