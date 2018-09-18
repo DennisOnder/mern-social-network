9c20c1f481bf137175b0e283808533b11e4525cf
Dennis Onder <webdev.dennisonder@gmail.com>
2018-09-18 22:33:39 +0200

    Commit: Added the ability to fetch profile data

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profileActions';
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+  render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <h4>Loading...</h4>
+    } else {
+      dashboardContent = <h1>Hello, {this.props.auth.user.name}</h1>
+    }
+
+    return (
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">
+                {dashboardContent}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
